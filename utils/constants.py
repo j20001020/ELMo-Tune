d7e803be93f72055859ead6a25348a213f004d16
j20001020 <j20001020@gmail.com>
2025-12-23 11:03:14 +0000

    修改output folder命名

--- utils/constants.py
+++ utils/constants.py
@@ -17,7 +17,7 @@ def path_of_output_folder():
     '''
     current_datetime = datetime.now()
     date_time_string = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-    output_folder_dir = f"output/output_{date_time_string}"
+    output_folder_dir = f"output/output_{date_time_string}_{TEST_NAME}"
 
     os.makedirs(output_folder_dir, exist_ok=True)
     print(f"[UTL] Using output folder: {output_folder_dir}")
@@ -27,7 +27,7 @@ def path_of_output_folder():
 # Check the environement variables, set to default if not found
 env_DEVICE = os.getenv("DEVICE", None)
 env_TEST_NAME = os.getenv("TEST_NAME", None)
-env_CASE_NUMBER = os.getenv("CASE_NUMBER", 1)
+# env_CASE_NUMBER = os.getenv("CASE_NUMBER", 1)
 env_VERSION = os.getenv("VERSION", "8.8.1")
 env_OUTPUT_PATH = os.getenv("OUTPUT_PATH", None)
 env_NUM_ENTRIES = os.getenv("NUM_ENTRIES", 30000000)
@@ -35,8 +35,8 @@ env_SIDE_CHECKER = os.getenv("SIDE_CHECKER", True)
 
 # Parse the arguments. They replace the environment variables if they are set
 parser = argparse.ArgumentParser(description='Description of your script')
-parser.add_argument('-c', '--case', type=int, default=env_CASE_NUMBER, help='Specify the case number')
-parser.add_argument('-d', '--device', type=str, default=env_DEVICE, help='Specify the device')
+# parser.add_argument('-c', '--case', type=int, default=env_CASE_NUMBER, help='Specify the case number')
+# parser.add_argument('-d', '--device', type=str, default=env_DEVICE, help='Specify the device')
 parser.add_argument('-t', '--workload', type=str, default=env_TEST_NAME, help='Specify the test name')
 parser.add_argument('-v', '--version', type=str, default=env_VERSION, help='Specify the version of RocksDB')
 parser.add_argument('-o', '--output', type=str, default=env_OUTPUT_PATH, help='Specify the output path')
@@ -44,8 +44,8 @@ parser.add_argument('-n', '--num_entries', type=int, default=env_NUM_ENTRIES, he
 parser.add_argument('-s', '--side_checker', type=bool, default=env_SIDE_CHECKER, help='Specify if side checker is enabled')
 
 args, unknown = parser.parse_known_args()
-CASE_NUMBER = args.case
-DEVICE = args.device
+# CASE_NUMBER = args.case
+# DEVICE = args.device
 TEST_NAME = args.workload
 VERSION = args.version
 OUTPUT_PATH = args.output if args.output else path_of_output_folder()
